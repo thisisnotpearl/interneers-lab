304184d87d656de51acfb48f591f059aabbe1890
Pearl Vashistha <vashisthapearl@gmail.com>
2026-02-16 14:29:03 +0530

    first-api in django- week 1

--- backend/python/django_app/urls.py
+++ backend/python/django_app/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse
+from django.http import JsonResponse
 
 def hello_world(request):
-    return HttpResponse("Hello, world! This is our interneers-lab Django server.")
+    name = request.GET.get("name", "World")
+    return JsonResponse({"message": f"Hello, {name}!"})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
